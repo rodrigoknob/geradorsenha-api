dc6ba777022e87a26962e3d859544560c1f3eea7
rodrigoknob <rodrigoknob@partithura.com.br>
2021-04-26 15:18:45 -0300

    permitindo requisicao a api

--- src/main/java/com/gerador_senhas/api_rest/ApiRestApplication.java
+++ src/main/java/com/gerador_senhas/api_rest/ApiRestApplication.java
@@ -3,12 +3,21 @@ package com.gerador_senhas.api_rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 
 @EntityScan("com.gerador_senhas.models")
-public class ApiRestApplication {
-
+@EnableWebMvc
+public class ApiRestApplication implements WebMvcConfigurer{
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") ;
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestApplication.class, args);
 	}

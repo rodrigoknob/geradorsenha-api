ce6a521b215e3c6bd4da101b7d7bdcf3bae685ea
rodrigoknob <rodrigoknob@partithura.com.br>
2021-04-27 14:32:27 -0300

    adicionando funcao para zerar contador no banco

--- src/main/java/com/gerador_senhas/api_rest/resources/SenhaResource.java
+++ src/main/java/com/gerador_senhas/api_rest/resources/SenhaResource.java
@@ -3,6 +3,8 @@ package com.gerador_senhas.api_rest.resources;
 import java.util.Date;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -72,8 +74,9 @@ public class SenhaResource {
 	}
 
 	@DeleteMapping("/senhas")
+	@Transactional
 	public void zerarSenhas() {
-		senhaRepository.deleteAll();
+		senhaRepository.zerarSenhas();
 	}
 
 	@PutMapping("/senhas")
--- src/main/java/com/gerador_senhas/api_rest/respository/SenhaRepository.java
+++ src/main/java/com/gerador_senhas/api_rest/respository/SenhaRepository.java
@@ -4,6 +4,7 @@ package com.gerador_senhas.api_rest.respository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import com.gerador_senhas.models.Senha;
@@ -16,4 +17,11 @@ public interface SenhaRepository extends JpaRepository<Senha, Long> {
 	
 	@Query("SELECT S from Senha as S where situacao = 'ESPERA' ORDER BY tipo DESC, data_geracao ASC")
 	List<Senha>findProximaSenha();
+	
+	@Modifying
+	@Query(
+			value="truncate TABLE Senha RESTART IDENTITY",
+			nativeQuery=true
+			)
+	void zerarSenhas();
 }
